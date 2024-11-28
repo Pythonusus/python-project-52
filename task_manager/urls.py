8f0f1357a46ee520154647ee915b776e3cf14c51
Petr Malafeev <StandBy545@yandex.ru>
2024-11-28 16:55:37 +0300

    Upd: urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -20,6 +20,9 @@ from django.urls import path
 from task_manager import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.IndexView.as_view(), name='index'),
+    path('users/', views.UsersView.as_view(), name='users'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
     path('admin/', admin.site.urls),
 ]
