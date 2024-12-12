343f68f0ee890b799c947327bc69160f2d17e591
Petr Malafeev <StandBy545@yandex.ru>
2024-12-12 12:31:59 +0300

    Upd: users CRUD

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,27 +1,90 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
+import task_manager.texts as texts
 from task_manager.users.forms import UserForm
 from task_manager.users.models import User
-from task_manager.texts import base, create_user
 
 
 class UsersIndexView(ListView):
     model = User
     template_name = 'users/index.html'
+    context_object_name = 'users'
     extra_context = {
-        'base': base,
+        'base': texts.base,
+        'users_index': texts.users_index,
     }
 
 
-class UserCreateView(CreateView, SuccessMessageMixin):
+class UserCreateView(SuccessMessageMixin, CreateView):
     model = User
     form_class = UserForm
     template_name = 'users/create.html'
     success_url = reverse_lazy('login')
-    success_message = create_user['register_success']
+    success_message = texts.create_user['register_success']
     extra_context = {
-        'base': base,
-        'create_user': create_user,
+        'base': texts.base,
+        'create_user': texts.create_user,
     }
+
+
+class UserUpdateView(SuccessMessageMixin,
+                     LoginRequiredMixin,
+                     PermissionRequiredMixin,
+                     UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = 'users/update.html'
+    permission_required = 'auth.change_user'
+
+    success_url = reverse_lazy('users_index')
+    success_message = texts.update_user['update_success']
+
+    extra_context = {
+        'base': texts.base,
+        'update_user': texts.update_user,
+    }
+
+    def has_permission(self):
+        return self.get_object().id == self.request.user.id
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, texts.auth['auth_required'])
+            return redirect(reverse_lazy('login'))
+
+        messages.error(self.request, texts.auth['permission_required'])
+        return redirect(reverse_lazy('users_index'))
+
+
+class UserDeleteView(SuccessMessageMixin,
+                     LoginRequiredMixin,
+                     PermissionRequiredMixin,
+                     DeleteView):
+    model = User
+    template_name = 'users/delete.html'
+    permission_required = 'auth.delete_user'
+
+    success_url = reverse_lazy('users_index')
+    success_message = texts.delete_user['delete_success']
+
+    extra_context = {
+        'base': texts.base,
+        'delete_user': texts.delete_user,
+    }
+
+    def has_permission(self):
+        return self.get_object().id == self.request.user.id
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, texts.auth['auth_required'])
+            return redirect(reverse_lazy('login'))
+
+        messages.error(self.request, texts.auth['permission_required'])
+        return redirect(reverse_lazy('users_index'))
