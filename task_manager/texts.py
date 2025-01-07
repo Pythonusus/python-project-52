f151c153f25778e3bf7ea8396dbcdc8b22969a19
Petr Malafeev <StandBy545@yandex.ru>
2025-01-07 20:35:23 +0300

    Upd: add i18n texts for statuses and tasks

--- task_manager/texts.py
+++ task_manager/texts.py
@@ -66,3 +66,73 @@ login = {
 logout = {
     'logout_info': _('You are logged out'),
 }
+
+# Texts for the statuses index page
+statuses_index = {
+    'statuses': _('Statuses'),
+    'create_status': _('Create status'),
+    'id': _('ID'),
+    'name': _('Name'),
+    'created_at': _('Created at'),
+    'edit': _('Edit'),
+    'delete': _('Delete'),
+}
+
+# Texts for the statuses create page
+create_status = {
+    'create_status': _('Create status'),
+    'create': _('Create'),
+    'create_success': _('Status created successfully'),
+}
+
+# Texts for the statuses update page
+update_status = {
+    'update_status': _('Update status'),
+    'update': _('Update'),
+    'update_success': _('Status updated successfully'),
+}
+
+# Texts for the statuses delete page
+delete_status = {
+    'delete_status': _('Delete status'),
+    'delete_confirm': _('Are you sure you want to delete this status?'),
+    'delete_anyway': _('Delete anyway'),
+    'delete_success': _('Status deleted successfully'),
+    'delete_error': _('Status cannot be deleted because it is in use'),
+}
+
+# Texts for the tasks index page
+tasks_index = {
+    'tasks': _('Tasks'),
+    'create_task': _('Create task'),
+    'id': _('ID'),
+    'name': _('Name'),
+    'status': _('Status'),
+    'author': _('Author'),
+    'executor': _('Executor'),
+    'created_at': _('Created at'),
+    'edit': _('Edit'),
+    'delete': _('Delete'),
+}
+
+# Texts for the tasks create page
+create_task = {
+    'create_task': _('Create task'),
+    'create': _('Create'),
+    'create_success': _('Task created successfully'),
+}
+
+# Texts for the tasks update page
+update_task = {
+    'update_task': _('Update task'),
+    'update': _('Update'),
+    'update_success': _('Task updated successfully'),
+}
+
+# Texts for the tasks delete page
+delete_task = {
+    'delete_task': _('Delete task'),
+    'delete_confirm': _('Are you sure you want to delete this task?'),
+    'delete_anyway': _('Delete anyway'),
+    'delete_success': _('Task deleted successfully'),
+}
